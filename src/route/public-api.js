d8d4623367d03f9c02485b28689e6ff525f713af
abdultalif <abdultalif85@gmail.com>
2023-11-28 07:18:07 +0700

    update

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -1,5 +1,7 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
+import menuController from "../controller/menu-controller.js";
+import testimoniController from "../controller/testimoni-controller.js";
 
 const publicRouter = new express.Router();
 publicRouter.post('/api/register', userController.register);
@@ -7,9 +9,9 @@ publicRouter.post('/api/login', userController.login);
 
 publicRouter.get('/api/testimoni', testimoniController.testimoni);
 
-publicRouter.get('/api/menu', menuController.menus);
-publicRouter.get('/api/menu/:menuId', menuController.menu);
-publicRouter.get('/api/cari', menuController.cari);
+publicRouter.get('/api-public/menu', menuController.menus);
+publicRouter.get('/api-public/menu/:menuId', menuController.menu);
+publicRouter.get('/api-public/cari', menuController.cari);
 
 export {
     publicRouter

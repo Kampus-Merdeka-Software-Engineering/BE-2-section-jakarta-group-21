fb9957e6e1e934182595a61389354857198afc04
abdultalif <abdultalif85@gmail.com>
2023-11-28 07:03:28 +0700

    mindahin endpoint

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -5,6 +5,12 @@ const publicRouter = new express.Router();
 publicRouter.post('/api/register', userController.register);
 publicRouter.post('/api/login', userController.login);
 
+publicRouter.get('/api/testimoni', testimoniController.testimoni);
+
+publicRouter.get('/api/menu', menuController.menus);
+publicRouter.get('/api/menu/:menuId', menuController.menu);
+publicRouter.get('/api/cari', menuController.cari);
+
 export {
     publicRouter
 }

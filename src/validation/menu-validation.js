f02cb49442d23cc21f45da4c1e0a471653f1f996
abdultalif <abdultalif85@gmail.com>
2023-12-08 20:21:45 +0700

    update validasi

--- src/validation/menu-validation.js
+++ src/validation/menu-validation.js
@@ -4,7 +4,7 @@ const createMenuValidation = Joi.object({
     name: Joi.string().max(100).required(),
     description: Joi.string().optional(),
     price: Joi.string().max(100).required(),
-    stok: Joi.string().max(100).required(),
+    stok: Joi.string().max(100).optional(),
     image: Joi.string().max(100).required().optional(),
     category: Joi.string().max(100).required(),
 });
